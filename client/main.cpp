476de160409f243bb95eb8ba302e1fc19e42b6c4
WangXvDong-bupt <68494698+WangXvDong-bupt@users.noreply.github.com>
2020-07-25 19:49:02 +0800

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -1,59 +1,59 @@
-// CMake_chat.cpp: 定义应用程序的入口点。
+// CMake_client.cpp: 定义应用程序的入口点。
 //
-
-#include "CMake_chat.h"
+#include "CMake_client.h"
 #include<iostream>
 #include<stack>
 #include<vector>
 #include<cstdio>
 #include<string>
+#include <process.h>
 #include<winsock2.h>
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable:4996)
 
 using namespace std;
+//using std::cin; using std::cout; using std::endl;
+
 #define maxSize 1024
-vector<SOCKET> sockets;
 
 
-DWORD WINAPI CreateClientThread(LPVOID lpParameter)
+DWORD WINAPI CreateSendMegThread(LPVOID lpParameter)
 {
-    SOCKET sock_clt = (SOCKET)lpParameter;
-    sockets.push_back(sock_clt);
-    char meg_recv[maxSize] = {};
-    //定义长度变量
-    int send_len = 0;
-    int recv_len = 0;
-    cout << "创建线程成功" << endl;
+    SOCKET s_send = (SOCKET)lpParameter;
     while (true)
     {
-        recv_len = recv(sock_clt, meg_recv, maxSize, 0);
-        cout << "接收消息：" << meg_recv << endl;
-        if (sockets.size() > 1)
-        {
-            if (sock_clt == sockets[0])
-            {
-                send_len = send(sockets[1], meg_recv, sizeof(meg_recv), 0);
-                cout << "发送消息至客户端:" << sockets[1] << endl;
-            }
-            if (sock_clt == sockets[1])
-            {
-                send_len = send(sockets[0], meg_recv, sizeof(meg_recv), 0);
-                cout << "发送消息至客户端:" << sockets[0] << endl;
-            }
-            if (send_len < 0)
-            {
-                cout << "发送失败" << endl;
-                break;
-            }
+        int send_len = 0;
+        cout << endl << "请输入发送信息:";
+        string line;
+        getline(cin, line);
+        send_len = send(s_send, line.c_str(), maxSize, 0);
+        if (send_len == SOCKET_ERROR) {
+            cout << "发送失败！" << endl;
+            continue;
         }
-        else cout << "当前只有一个客户端" << endl;
-        memset(meg_recv, 0, sizeof(meg_recv));      //清空数据缓冲区
+
     }
-    closesocket(sock_clt);
     return 0;
 }
 
+DWORD WINAPI CreateRecvMegThread(LPVOID lpParameter)
+{
+    SOCKET s_recv = (SOCKET)lpParameter;
+    while (true)
+    {
+        char meg_recv[maxSize] = {};
+        int recv_len = 0;
+        recv_len = recv(s_recv, meg_recv, maxSize, 0);
+        if (recv_len == SOCKET_ERROR) {
+            cout << "接受失败！" << endl;
+            continue;
+        }
+        else {
+            cout << endl << "服务端信息:" << meg_recv << endl;
+        }
+    }
+    return 0;
+}
 
 int main()
 {
@@ -69,74 +69,57 @@ int main()
         cout << "初始化套接字库成功！" << endl;
     }
 
-    //定义长度变量
-    int send_len = 0;
-    int recv_len = 0;
-    int len = 0;
-    //定义发送缓冲区和接受缓冲区
-    char send_buf[maxSize];
-    char recv_buf[maxSize];
-    //定义服务端套接字，接受请求套接字
-    SOCKET socketServer;
-    SOCKET socketAccept;
-    //服务端地址客户端地址
-    SOCKADDR_IN server_addr;                 //一种通用的套接字地址。
-    SOCKADDR_IN accept_addr;
 
+
+    //定义服务端套接字
+    SOCKET socketServer;
+    //服务端地址
+    SOCKADDR_IN server_addr;
     //填充服务端信息
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ADDR_ANY
-    server_addr.sin_port = htons(1234);                  //传递端口 htons：将主机字节顺序转换为网络字节顺序
+    server_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(1234);
     //创建套接字
-    socketServer = socket(AF_INET, SOCK_STREAM, 0);         //ADDRESS FAMILY协议族，流式套接字类型，
-    if (bind(socketServer, (SOCKADDR*)&server_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
-        cout << "套接字绑定失败！" << endl;
+    socketServer = socket(AF_INET, SOCK_STREAM, 0);
+    if (connect(socketServer, (SOCKADDR*)&server_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
+        cout << "服务器连接失败！" << endl;
         WSACleanup();
     }
     else {
-        cout << "套接字绑定成功！" << endl;
+        cout << "服务器连接成功！" << endl;
     }
-    //设置套接字为监听状态
-    if (listen(socketServer, SOMAXCONN) < 0) {
-        cout << "设置监听状态失败！" << endl;
-        WSACleanup();
-    }
-    else {
-        cout << "设置监听状态成功！" << endl;
-    }
-    cout << "服务端正在监听连接，请稍候...." << endl;
-    //接受连接请求
-    /*len = sizeof(SOCKADDR);
-    socketAccept = accept(socketServer, (SOCKADDR*)&accept_addr, &len);
-    if (socketAccept == SOCKET_ERROR) {
-        cout << "连接失败！" << endl;
-        WSACleanup();
-        return 0;
-    }*/
-    cout << "连接建立，准备接受数据" << endl;
-    //接收数据
+
     while (true)
     {
-        len = sizeof(SOCKADDR);
-        socketAccept = accept(socketServer, (SOCKADDR*)&accept_addr, &len);
-        if (socketAccept == SOCKET_ERROR) {
-            cout << "连接失败！" << endl;
-            WSACleanup();
-            return 0;
+        cout << "请输入用户名" << endl;
+        char username[20] = {};
+        cin.getline(username, 20);
+        cout << "请输入密码" << endl;
+        char password[20] = {};
+        cin.getline(password, 20);
+        if (strcmp(username, "ww") == 0 && strcmp(password, "12") == 0)
+        {
+            cout << "登录成功" << endl;
+            break;
         }
-        auto sock_thread = ::CreateThread(nullptr, 0, CreateClientThread, (LPVOID)socketAccept, 0, nullptr);
-        if (sock_thread == NULL) {
-            cerr << "创建线程失败! Error code: " << ::WSAGetLastError() << endl;
-            WSACleanup();
-            //system("pause");
-            return 0;
+        else if (strcmp(username, "xx") == 0 && strcmp(password, "34") == 0)
+        {
+            cout << "登录成功" << endl;
+            break;
         }
-        ::CloseHandle(sock_thread);
+        else
+        {
+            cout << "登录失败" << endl;
+        }
     }
-    //关闭套接字
+    CreateThread(nullptr, 0, CreateRecvMegThread, (LPVOID)socketServer, 0, nullptr);
+    CreateThread(nullptr, 0, CreateSendMegThread, (LPVOID)socketServer, 0, nullptr);
+    //_beginthread(recvMeg, 0, &socketServer);
+    //_beginthread(sendMeg, 0, &socketServer);
+    while (true) {}
+
     closesocket(socketServer);
-    closesocket(socketAccept);
-    //释放DLL资源
     WSACleanup();
+
     return 0;
 }
