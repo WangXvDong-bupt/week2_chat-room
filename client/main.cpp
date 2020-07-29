0a052fce559fbea6f5e28090fdcd15146b01e4e3
WangXvDong-bupt <68494698+WangXvDong-bupt@users.noreply.github.com>
2020-07-29 15:15:53 +0800

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -10,23 +10,64 @@
 #include<winsock2.h>
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable:4996)
-
 using namespace std;
-//using std::cin; using std::cout; using std::endl;
-
 #define maxSize 1024
 
+//定义储存用户ID的数据类型
+typedef struct userInformition
+{
+    string username;
+    string password;
+}node_user;
+//定义向量用于存储用户ID信息
+vector<node_user> user_login = { {"ww","12"},{"xx","34"} };
+
+string Login(const vector<node_user> &user_login)
+{
+    string username;
+    string password;
+    while (true)
+    {
+        cout << "请输入用户名" << endl;
+        cin >> username;
+        if (username != "ww" && username != "xx")
+        {
+            cout << "该用户不存在" << endl;
+            continue;
+        }
+        cout << "请输入密码" << endl;
+        cin >> password;
+        bool login = false;
+        for (auto c : user_login)
+        {
+            if (username == c.username && password == c.password)
+            {
+                cout << "登录成功" << endl;
+                login = true;
+                break;
+            }
+        }
+        if (login) break;
+        else {
+            cout << "登录失败" << endl;
+            continue;
+        }
+    }
+    return username;
+}
 
 DWORD WINAPI CreateSendMegThread(LPVOID lpParameter)
 {
     SOCKET s_send = (SOCKET)lpParameter;
+    cin.ignore();
     while (true)
     {
         char meg_send[maxSize] = {};
         int send_len = 0;
-        cout << endl << "请输入发送信息:";
+        cout << "请输入发送信息（向其他用户发信息需键入‘:’及用户名）:";
+        //string line;
         cin.getline(meg_send, maxSize);
-        cout << "in:" << meg_send << endl;
+        //cout << "in:" << meg_send << endl;
         send_len = send(s_send, meg_send, maxSize, 0);
         if (send_len == SOCKET_ERROR) {
             cout << "发送失败！" << endl;
@@ -59,7 +100,7 @@ DWORD WINAPI CreateRecvMegThread(LPVOID lpParameter)
 int main()
 {
     //初始化套接字库
-    WORD w_req = MAKEWORD(2, 2);//版本号
+    WORD w_req = MAKEWORD(2, 2);//版本号2.2
     WSADATA wsadata;
     //int err;
     //err = WSAStartup(w_req, &wsadata);
@@ -89,34 +130,27 @@ int main()
     else {
         cout << "服务器连接成功！" << endl;
     }
-
-    while (true)
+    string userID;
+    int send_len;
+    userID = Login(user_login);                     //调用登录函数
+    while (true)                                    //向服务器发送登录信息
     {
-        cout << "请输入用户名" << endl;
-        char username[20] = {};
-        cin.getline(username, 20);
-        cout << "请输入密码" << endl;
-        char password[20] = {};
-        cin.getline(password, 20);
-        if (strcmp(username, "ww") == 0 && strcmp(password, "12") == 0)
-        {
-            cout << "登录成功" << endl;
-            break;
-        }
-        else if (strcmp(username, "xx") == 0 && strcmp(password, "34") == 0)
-        {
-            cout << "登录成功" << endl;
-            break;
+        char currentID[maxSize] = {};
+        strcpy(currentID, userID.c_str());
+        send_len = send(socketServer, currentID, maxSize, 0);
+        if (send_len == SOCKET_ERROR) {
+            cout << currentID << "用户登录服务器失败！"  << endl;
+            continue;
         }
         else
         {
-            cout << "登录失败" << endl;
+            cout << currentID << "用户登录服务器成功！" << endl;
+            break;
         }
     }
+    
     CreateThread(nullptr, 0, CreateRecvMegThread, (LPVOID)socketServer, 0, nullptr);
     CreateThread(nullptr, 0, CreateSendMegThread, (LPVOID)socketServer, 0, nullptr);
-    //_beginthread(recvMeg, 0, &socketServer);
-    //_beginthread(sendMeg, 0, &socketServer);
     while (true) {}
 
     closesocket(socketServer);
